b04d948bac480e08354680572abd4292808beaaa
rodvos <robinraposa@gmail.com>
2016-10-27 18:46:05 +0300

    Tagging feature completed

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -42,7 +42,7 @@ class ArticlesController < ApplicationController
 	
 	private
 	def article_params
-		params.require(:article).permit(:title, :body)
+		params.require(:article).permit(:title, :body, :tag_list)
 	end
 		
 end
